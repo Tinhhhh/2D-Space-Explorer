1f29197feac8c2e4d3af5c621b6a976c7334b4c0
tuankiet12012003 <kietdhtse172938@fpt.edu.vn>
2024-06-17 11:44:07 +0700

    Winning Game

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -38,9 +38,22 @@ public class PlayerManager : MonoBehaviour
            UIManager.Instance.Restart.SetActive(true);
            UIManager.Instance.Exit.SetActive(true);
            UIManager.Instance.MainMenu.SetActive(true);
+
+            //Pause Button Disappear When Game Over Appear
            pauseButton.SetActive(false);
         }
+
+        /*if (Winning Condition)
+        {
+            UIManager.Instance.WinningTitle.SetActive(true);
+            UIManager.Instance.WinningExit.SetActive(true);
+            UIManager.Instance.WinningStartGame.SetActive(true);
+            UIManager.Instance.WinningMainMenu.SetActive(true);
+
+            //Pause Button Disappear When Winning Appear
+            pauseButton.SetActive(false);
+        }*/
     }
 
-    
+
 }
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    //GameOverObject
     public GameObject Title;
     public GameObject Exit;
     public GameObject Restart;
     public GameObject MainMenu;
+
+    //Winnning Object
+    public GameObject WinningTitle;
+    public GameObject WinningExit;
+    public GameObject WinningStartGame;
+    public GameObject WinningMainMenu;
 
     static public UIManager Instance;
     // Start is called before the first frame update
     private void Awake()
     {
         UIManager.Instance = this;
+        //GameOver Disappear When Game Start
         this.Title = GameObject.Find("Title");
         this.Exit = GameObject.Find("Exit");
         this.Restart = GameObject.Find("Restart");
@@ -22,5 +30,15 @@ public class UIManager : MonoBehaviour
         this.Exit.SetActive(false);
         this.Restart.SetActive(false);
         this.MainMenu.SetActive(false);
+
+        //Winning Disappear When Game Start
+        this.WinningTitle = GameObject.Find("WinningTitle");
+        this.WinningExit = GameObject.Find("WinningExit");
+        this.WinningStartGame = GameObject.Find("WinningStartGame");
+        this.WinningMainMenu = GameObject.Find("WinningMainMenu");
+        this.WinningTitle.SetActive(false);
+        this.WinningExit.SetActive(false);
+        this.WinningStartGame.SetActive(false);
+        this.WinningMainMenu.SetActive(false);
     }
 }
